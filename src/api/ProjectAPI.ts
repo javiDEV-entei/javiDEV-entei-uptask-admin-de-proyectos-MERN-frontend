d711c1f54a1089415f48c8f8bffb14ac44f8356f
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-08-27 15:57:14 -0300

    archivos para hacer las peticiones a la API agregados

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,7 +1,20 @@
-import { data } from "react-router-dom";
+import api from "@/lib/axios";
+import type { ProjectFormData } from "../types";
 
-export async function createProject(){
 
-    console.log(data);
+
+export async function createProject(formData: ProjectFormData){
+
+    try {
+        const {data} = await api.post('/projects', formData)
+        console.log(data);
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+        
+    }
+    
     
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,8 +1,8 @@
 import { Link} from 'react-router-dom'
 import {useForm} from 'react-hook-form'
 import ProjectForm from '@/components/projects/ProjectForm'
-import type { ProjectFormData } from 'types'
-
+import type { ProjectFormData } from '@/types/index'
+import { createProject } from '@/api/ProjectAPI'
 
 export default function CreateProjectView() {
 
@@ -16,7 +16,7 @@ export default function CreateProjectView() {
     const {register, handleSubmit, formState:{errors}} = useForm<ProjectFormData>({defaultValues:initialValues})
 
     const handleForm = (data: ProjectFormData) =>{
-        console.log(data);
+       createProject(data);
         
     }
   return (
