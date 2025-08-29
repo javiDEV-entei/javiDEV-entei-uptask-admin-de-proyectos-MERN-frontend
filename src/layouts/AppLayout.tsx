7451d79021c32d768bbfd32740ab9df39df8134b
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-08-29 17:19:38 -0300

    dependencias de react-toastify y react query agregadas

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,4 +1,6 @@
 import {Outlet} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Logo from '@/components/Logo'
 import NavMenu from '@/components/NavMenu'
 
@@ -26,6 +28,13 @@ export default function AppLayout() {
             </p>
 
         </footer>
+
+        <ToastContainer
+            pauseOnHover={false}
+            pauseOnFocusLoss={false}
+        
+        
+        />
 
 
     </>
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate} from 'react-router-dom'
 import {useForm} from 'react-hook-form'
+import {toast} from 'react-toastify'
 import ProjectForm from '@/components/projects/ProjectForm'
 import type { ProjectFormData } from '@/types/index'
 import { createProject } from '@/api/ProjectAPI'
@@ -16,8 +17,9 @@ export default function CreateProjectView() {
 
     const {register, handleSubmit, formState:{errors}} = useForm<ProjectFormData>({defaultValues:initialValues})
 
-    const handleForm = async (data: ProjectFormData) =>{
-       await createProject(data)
+    const handleForm = async (formData: ProjectFormData) =>{
+       const data = await createProject(formData)
+       toast.success(data)
        navigate('/')
         
     }
