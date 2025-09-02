cfb9b318180aebf84b7ebc06f404d87ecca31039
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-09-02 07:03:38 -0300

    dashboard de proyectos agregado
    se realizaron los primeros pasos para generar un vista preeliminar del dashboard de proyectos

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate} from 'react-router-dom'
 import {useForm} from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query'
 import {toast} from 'react-toastify'
 import ProjectForm from '@/components/projects/ProjectForm'
 import type { ProjectFormData } from '@/types/index'
@@ -17,12 +18,22 @@ export default function CreateProjectView() {
 
     const {register, handleSubmit, formState:{errors}} = useForm<ProjectFormData>({defaultValues:initialValues})
 
-    const handleForm = async (formData: ProjectFormData) =>{
-       const data = await createProject(formData)
-       toast.success(data)
+    const {mutate} = useMutation({
+      mutationFn:createProject,
+      onError: (error) =>{
+          toast.error(error.message);
+          
+        },
+        onSuccess: (data) =>{
+          toast.success(data)
        navigate('/')
+      }
+    })
+
+    const handleForm = (formData: ProjectFormData) => mutate(formData)
+       
         
-    }
+    
   return (
     <>
     <div className='max-w-3xl mx-auto'>
