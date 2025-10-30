35a7f26731b566fd42c2bce6ba8ef7998c8c431f
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-10-30 07:10:16 -0300

    varias vistas y funciones agregadas
    se agregaron las vistas y funciones para: creacion de cuentas de usuario, confirmacion de cuenta y login de usuarios. Se agregaron los tipos y schemas correspondientes a las funciones

---
+++ src/api/AuthAPI.ts
@@ -0,0 +1,24 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { type UserRegistrationForm } from "../types";
+
+
+export async function createAccount(formData: UserRegistrationForm){
+
+    try {
+
+        const url ='/auth/create-account'
+        const {data} = await api.post<string>(url, formData)
+        return data
+        
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+            
+        }
+        
+    }
+}
+
+
+//http://localhost:4000/api/auth/confirm-account
--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,10 @@ import DashbordView from '@/views/DashbordView'
 import CreateProjectView from './views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
 import ProjectDetailsView from './views/projects/ProjectDetailsView'
+import AuthLayout from './layouts/AuthLayout'
+import LoginView from './views/auth/LoginView'
+import RegisterView from './views/auth/RegisterView'
+import ConfirmAccountView from './views/auth/ConfirmAccountView'
 
 export default function Router (){
 
@@ -17,6 +21,15 @@ export default function Router (){
                     <Route path='/projects/:projectId' element={<ProjectDetailsView/>}/>
                     <Route path='/projects/:projectId/edit' element={<EditProjectView/>}/>
                     
+                </Route>
+
+
+
+                <Route element={<AuthLayout/>}>
+                    <Route path='/auth/login' element={<LoginView/>}/>
+                    <Route path='/auth/register' element={<RegisterView/>}/>
+                    <Route path='/auth/confirm-account' element={<ConfirmAccountView/>}/>
+
                 </Route>
             </Routes>
         </BrowserRouter>
