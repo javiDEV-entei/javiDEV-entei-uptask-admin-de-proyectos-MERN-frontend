c50d3899733e6b3de4caf4d4f42eafd7f889a5b2
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-11-01 14:18:05 -0300

    funciones de login y autenticacion agregadas

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { type UserRegistrationForm } from "../types";
+import { type ConfirmToken, type RequestConfirmationCodeForm, type UserLoginForm, type UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData: UserRegistrationForm){
@@ -19,6 +19,53 @@ export async function createAccount(formData: UserRegistrationForm){
         
     }
 }
+export async function confirmAccount(formData: ConfirmToken){
+
+    try {
+
+        const url ='/auth/confirm-account'
+        const {data} = await api.post<string>(url, formData)
+        return data
+        
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+            
+        }
+        
+    }
+}
+export async function requestConfirmatinCode(formData: RequestConfirmationCodeForm){
+
+    try {
+
+        const url ='/auth/request-code'
+        const {data} = await api.post<string>(url, formData)
+        return data
+        
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+            
+        }
+        
+    }
+}
+export async function authenticateUser(formData: UserLoginForm){
+
+    try {
+
+        const url ='/auth/login'
+        const {data} = await api.post<string>(url, formData)
+        return data
+        
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+            
+        }
+        
+    }
+}
 
 
-//http://localhost:4000/api/auth/confirm-account
--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {PinInput, PinInputField} from '@chakra-ui/pin-input'
-import { useState } from "react";
+import {useMutation} from '@tanstack/react-query'
 import type { ConfirmToken } from "@/types/index";
+import { confirmAccount } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
 
 export default function ConfirmAccountView() {
   const [token, setToken] = useState<ConfirmToken['token']>('')
+
+  const {mutate} = useMutation({
+    mutationFn:confirmAccount,
+
+
+    onError:(error) =>{
+        toast.error(error.message)
+    },
+    onSuccess: (data) =>{
+      toast.success(data)
+    }
+
+  })
 
   const handleChange = (token: ConfirmToken['token']) =>{
     setToken(token)
@@ -12,9 +28,7 @@ export default function ConfirmAccountView() {
 
 
 
-  const handleComplete = (token: ConfirmToken['token']) =>{
-    setToken(token)
-  }
+  const handleComplete = (token: ConfirmToken['token']) => mutate({token})
 
 
   return (
@@ -46,7 +60,7 @@ export default function ConfirmAccountView() {
 
       <nav className="mt-10 flex flex-col space-y-4">
         <Link
-          to='/auth/new-code'
+          to='/auth/request-code'
           className="text-center text-gray-300 font-normal"
         >
           Solicitar un nuevo Código
