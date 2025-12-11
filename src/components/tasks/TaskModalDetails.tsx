f7d5a2c826461bceec940fdee48729a40bd55a32
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-12-11 14:49:48 -0300

    historial de cambios agregado en pantalla
    ahora si muestra un listado de cambios en el modal de tareas con el nombre del colaborador que la modifico y a que estado

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -96,6 +96,17 @@ export default function TaskModalDetails() {
                                     >{data.name}
                                     </Dialog.Title>
                                     <p className='text-lg text-slate-500 mb-2'>Descripción:{data.description}</p>
+                                    <p className='text-lg text-slate-500 mb-2'>Historial de Cambios</p>
+
+                                   <ul className=' list-decimal'>
+                                     {data.completedBy.map((acttivityLog) =>(
+                                        <li key={acttivityLog._id}>
+                                            <span className=' font-bold text-slate-600'>
+                                            {statusTranslations [acttivityLog.status]}
+                                            </span>{' '} por: {acttivityLog.user.name}
+                                        </li>
+                                    ))}
+                                   </ul>
                                     <div className='my-5 space-y-3'>
                                         <label className='font-bold'>Estado Actual:</label>
 
